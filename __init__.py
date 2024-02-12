e87d4a0a17c0e354acd185c493ae9061f8e97eaf
Maissa95 <72620060+Maissa95@users.noreply.github.com>
2024-02-12 10:54:26 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template_string, render_template, jsonify
+import requests
 from flask import render_template
 from flask import json
 from datetime import datetime
@@ -6,6 +7,41 @@ from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
 app = Flask(__name__)  
+
+# Fonction pour récupérer les données sur les commits depuis l'API GitHub
+def get_commit_data():
+    url = 'https://api.github.com/repos/OpenRSI/5MCSI_Metriques/commits'
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        return None
+
+# Fonction pour extraire les minutes d'une date formatée
+@app.route('/extract-minutes/<date_string>')
+def extract_minutes(date_string):
+    date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
+    minutes = date_object.minute
+    return jsonify({'minutes': minutes})
+
+# Route pour afficher les données des commits
+@app.route('/commits/')
+def commits():
+    commit_data = get_commit_data()
+    if commit_data:
+        commits_per_minute = {}
+        for commit in commit_data:
+            date_string = commit['commit']['author']['date']
+            minutes = extract_minutes(date_string).json['minutes']
+            if minutes in commits_per_minute:
+                commits_per_minute[minutes] += 1
+            else:
+                commits_per_minute[minutes] = 1
+        return jsonify(commits_per_minute)
+    else:
+        return "Erreur lors de la récupération des données sur les commits."
+
 
 
 @app.route('/')
