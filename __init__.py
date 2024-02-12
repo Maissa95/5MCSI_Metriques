dbc015cf4fc54726acb181dcde8d1a401c9b6b80
Maissa95 <72620060+Maissa95@users.noreply.github.com>
2024-02-12 11:12:34 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template_string, render_template, jsonify
-#import requests
 from flask import render_template
 from flask import json
 from datetime import datetime
+import requests
 from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
